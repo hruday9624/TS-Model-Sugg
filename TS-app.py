0dc9f07ca4d27918a7783a114113db458a32dc42
Hruday Mouly <hrudaymouly96@gmail.com>
2024-10-09 23:19:55 +0530

    Update TS-app.py

--- TS-app.py
+++ TS-app.py
@@ -29,9 +29,9 @@ def suggest_forecasting_methods(time_series, freq):
         st.warning("The data is non-stationary.")
 
     try:
-        if len(time_series) < freq:
+        if len(time_series.dropna()) < freq:
             raise ValueError("The length of the time series is less than the specified frequency. Please provide more data or reduce the frequency.")
-        decomposition = seasonal_decompose(time_series, model='additive', period=freq)
+        decomposition = seasonal_decompose(time_series.dropna(), model='additive', period=freq)
         trend = decomposition.trend
         seasonal = decomposition.seasonal
         residual = decomposition.resid
